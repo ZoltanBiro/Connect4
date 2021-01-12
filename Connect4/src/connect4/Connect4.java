61f9a8e609450bf142878d329bf9122c1cb09f47
ZoltanBiro <69026364+ZoltanBiro@users.noreply.github.com>
2021-01-12 12:42:25 -0500

    Add files via upload

--- Connect4/src/connect4/Connect4.java
+++ Connect4/src/connect4/Connect4.java
@@ -5,7 +5,7 @@ public class Connect4 {
 
     public static void main(String[] args) {
         System.out.println("olah world");
-        System.out.println("whats up ms. luce");
+        System.out.println("how's it going");
     }
     
 }
